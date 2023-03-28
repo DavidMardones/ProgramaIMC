26ced775a026d1e29af92ab3c4699fc548399778
David Mardones <david.mardones04@inacapmail.cl>
2023-03-28 12:31:07 -0300

    calcularIMC

--- AdminPersonasModel/DTO/Persona.cs
+++ AdminPersonasModel/DTO/Persona.cs
@@ -1,28 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ProgramaIMC.DTO
 {
+    public struct IMC
+    {
+        public double Valor { get; set; }
+
+        public String Texto
+        {
+            get
+            {
+                if(Valor < 20)
+                {
+                    return "Anorexico";
+                } 
+                else if(Valor >= 20 && Valor <= 30)
+                {
+                    return "Gordo";
+                }
+                else
+                {
+                    return "El amigo de las historietas";
+                }
+            }
+        }
+    }
     public class Persona
     {
         private string nombre;
         private uint telefono;
         private double peso;
         private double estatura;
+        private IMC imc;
 
         public string Nombre { get => nombre; set => nombre = value; }
         public uint Telefono { get => telefono; set => telefono = value; }
         public double Peso { get => peso; set => peso = value; }
         public double Estatura { get => estatura; set => estatura = value; }
 
-        public double IMC
+        
+        public void calcularImc()
+        {
+            double valor = Peso / (Estatura * Estatura);
+            this.imc = new IMC() { Valor = valor };
+        }
+
+        public IMC IMC
         {
             get
             {
-                return Peso / (Estatura * Estatura);
+                return this.imc;
             }
         }
     }
